454af76f554a8b79dff1d0fa0b3bb031f1b97160
Aziz Rihani <mohamedaziz.rihani@esprit.tn>
2025-03-06 06:14:24 +0100

    Validation constraints

---
+++ src/main/java/com/ghoul/AmanaFund/entity/FrequencyPolice.java
@@ -0,0 +1,5 @@
+package com.ghoul.AmanaFund.entity;
+
+public enum FrequencyPolice {
+    MONTHLY,QUARTERLY,HALF_YEARLY,ANNUAL
+}
--- src/main/java/com/ghoul/AmanaFund/entity/Garantie.java
+++ src/main/java/com/ghoul/AmanaFund/entity/Garantie.java
@@ -1,6 +1,7 @@
 package com.ghoul.AmanaFund.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,8 @@ public class Garantie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idGarantie;
+    @NotNull(message = "Status should have value")
+    private boolean Active=false;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "police_id", nullable = false)
     private Police police;
