fb0d9777cd5bb6a6fb863da8948b1caac2ec6991
skander <mohamedskander.ajengui@esprit.tn>
2025-03-06 18:05:21 +0100

    API PDF,EXCEL et sms

--- src/main/java/com/ghoul/AmanaFund/service/SMSsinistre.java
+++ src/main/java/com/ghoul/AmanaFund/service/SMSsinistre.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Service;
 public class SMSsinistre {
 
     // Remplace par tes informations Twilio
-    public static final String ACCOUNT_SID = "AC2dc0420851c9d2db7a1301891bc952ae";
-    public static final String AUTH_TOKEN = "7fd929dd0ee0a1c06efae4f463c870a2";
+    public static final String ACCOUNT_SID = "TWILIO_SID";
+    public static final String AUTH_TOKEN = "TWILIO_AUTH_TOKEN";
     public static final String FROM_PHONE_NUMBER = "+18066029452"; // exemple : +1234567890
 
     public SMSsinistre() {
